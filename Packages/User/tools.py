9cdfd0182d43e02f3fcfba079d832fbefd11fec4
linkhanfeng <linkhanfeng@gmail.com>
2020-05-27 10:48:47 +0800

    add user

---
+++ Packages/User/tools.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+import sublime
+import sublime_plugin
+import datetime
+import os
+import urllib
+import yaml
+import re
+import platform
+import webbrowser
+import json
+# 公用方法
+
+
+def fileInfoObj(filepath):
+    path_name, ext_dot = os.path.splitext(filepath)
+    path, name = path_name.rsplit('\\', 1)
+    ext = ext_dot.replace('.', '', 1)
+    return {
+        'path': path,               # /user
+        'name': name,               # a
+        'ext_dot': ext_dot,         # .txt
+        'ext': ext,                 # txt
+        'name_ext': name + ext_dot,  # a.txt
+    }
+
+# 获取当前日期
+
+
+class GetDateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        t = datetime.datetime.now()
+        curIsoTime = t.isoformat(' ').split('.', maxsplit=1)[0]
+        selList = self.view.sel()
+        for selRegion in selList:
+            self.view.insert(edit, selRegion.a, curIsoTime)
+
+# 在浏览器中打开
+
+
+class OpenInBrowerCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **kwargs):
+        path = kwargs.get('url', None) if kwargs.get(
+            'url', None) else self.view.file_name()
+        path = 'file://' + path
+        webbrowser.open_new_tab(path)
+
+# 打开 js 文档
+
+
+class OpenJsDocCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # jsDocPath = 'file://' + sublime.packages_path() + '/User/MySubl/wangdoc-js/index.html'
+        pPath = 'file://' + sublime.packages_path()
+        jsDocPath = pPath + '/User/MySubl/wangdoc-js/index.html'
+        webbrowser.open_new_tab(jsDocPath)
+
+# 打开 Finder
+
+
+class OpenFinderCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        path = os.path.expanduser('~/it/')
+        sublime.active_window().run_command("open_dir", {"dir": path})
+
+# 打开 Sublime User 目录
+
+
+class OpenUserDirCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        path = sublime.packages_path() + '/User'
+        sublime.active_window().run_command("open_dir", {"dir": path})
+
+# 打开补全文件
+
+
+class OpenCompletionsFileCommand(sublime_plugin.WindowCommand):
+
+    def run(self):
+        # view = self.window.active_view()
+        path = sublime.packages_path() + '/User/MySubl/js.sublime-completions'
+        self.window.open_file(path)
+
+# 打开tips文件
+
+
+class OpenTipsCommand(sublime_plugin.WindowCommand):
+
+    def run(self):
+        # view = self.window.active_view()
+        path = '/Users/hf/it/note/dev/developer/tips.md'
+        self.window.open_file(path)
+
+# 将选择的文本转换为 completions
+
+
+class SelToCompletionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        letters = {'aa': '1', 'bb': '2', 'cc': '3', 'dd': '4',
+                   'ee': '5', 'ff': '6', 'gg': '7', 'hh': '8', 'ii': '9'}
+        view = self.view
+        sel = view.sel()
+        selTxt = []
+        for region in sel:
+            txt = view.substr(region)
+            selTxt.append(txt)
+            # print(111, txt)
+        selStr = json.dumps(selTxt[0], ensure_ascii=False)
+        selStr = selStr.replace('  ', '\\t')
+
+        # print(333, selStr)
+        def dashrepl(matchobj):
+            mm = matchobj.group(0)
+            # print(3, mm)
+            if mm:
+                return '${' + letters[mm] + ':}'
+        selStrb = re.sub(
+            'a{2}|b{2}|c{2}|d{2}|e{2}|f{2}|g{2}|h{2}|i{2}', dashrepl, selStr)
+        # print(444, selStrb)
+        comObj = {
+            'trigger': 'TTT',
+            'contents': selStrb.strip('"')
+        }
+        # print(555, comObj)
+        comStr = json.dumps(comObj, ensure_ascii=False)
+        comStr = comStr.replace('\\t', 't')
+        comStr = comStr.replace('\\n', 'n')
+        comStr = comStr + ','
+        sublime.set_clipboard(comStr)
---
+++ Packages/User/translate.py
@@ -0,0 +1,76 @@
+import json
+import urllib
+import re
+import hashlib
+from string import Template
+
+import sublime
+import sublime_plugin
+
+
+class GoogleTranslateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        def sendRequest(url, params):
+            pstr = ''
+            for k,v in params.items():
+                fixstr = ('?' if pstr == '' else '&')
+                pstr =  pstr + fixstr + k +  '=' + v
+            qUrl = url + pstr
+
+            proxy_handler = urllib.request.ProxyHandler({})
+            opener = urllib.request.build_opener(proxy_handler)
+            with opener.open(qUrl) as f:
+                data = f.read()
+                jsonResultString = data.decode()
+                jsonResult = json.loads(jsonResultString)
+                pass
+            return jsonResult
+
+        selected_point = self.view.sel()[0]
+        # 查询单词
+        s_word = self.view.substr(selected_point)
+        isEn = re.findall('[a-zA-Z0-9]+',s_word)
+
+        # 百度
+        url_baidu = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+        appid = '20180305000131189'
+        salt = '1234salt56'
+        passKey = 'ktmJAzaP6htDBalyfUFX'
+        signStr = '20180305000131189' + s_word + salt + passKey
+        signStrMd5 = hashlib.md5(signStr.encode(encoding='UTF-8')).hexdigest()
+        params_baidu = {
+            'q': urllib.parse.quote(s_word),
+            'from': "en" if isEn else "zh",
+            'to': "zh" if isEn else "en",
+            'appid': appid,
+            'salt': salt,
+            'sign': signStrMd5,
+        }
+        res_baidu = sendRequest(url_baidu, params_baidu)
+
+        url_google = 'http://translate.google.cn/translate_a/single'
+        params_google = {
+            'client':'gtx',
+            'sl': "en" if isEn else "zh-CN",
+            'tl': "zh-CN" if isEn else "en",
+            'dt':'t',
+            'q':urllib.parse.quote(s_word)
+        }
+        res_google = sendRequest(url_google, params_google)
+
+        baidu_word = ''
+        baidu_word_html = ''
+        for item in res_baidu['trans_result']:
+            baidu_word = baidu_word + item['dst']
+            baidu_word_html = baidu_word_html + '<p style="margin: 4px;">' + item['dst'] + '</p>'
+
+        google_word = ''
+        google_word_html = ''
+        for item in res_google[0]:
+            google_word = google_word + item[0]
+            google_word_html = google_word_html + '<p style="margin: 4px;">' + item[0] + '</p>'
+
+        sublime.set_clipboard(baidu_word.lower())
+
+        popupHtml = '<div style="border:1px solid #ccc; padding: 0 5px;"><span style="font-size:12px;margin:0;">百度:</span>' + baidu_word_html + '</div>' + '<div style="border:1px solid #ccc; padding: 0 5px;"><span style="font-size:12px;margin:0;">谷歌:</span>' + google_word_html + '</div>'
+        self.view.show_popup(popupHtml, sublime.HIDE_ON_MOUSE_MOVE_AWAY, -1, 600, 500)
